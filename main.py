4b73ce926769f1775d1b832d02b0f7fad23d2cdb
Afif Deshmukh <60255809+afieif@users.noreply.github.com>
2023-10-28 17:08:44 +0530

    Update main.py for Cumulative Waiting Timr

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import cvzone
 import math
 from sort import *
 from ultralytics import YOLO
+import time
 
 # Constants
 CLASS_NAMES = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
@@ -17,7 +18,6 @@ CLASS_NAMES = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "tra
                "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
                "teddy bear", "hair drier", "toothbrush"
                ]
-
 
 # ids
 # car - 2
@@ -35,15 +35,8 @@ def get_weight(clas):
     else:
         return 1
 
-
 def extract_weights(detections):
     return detections[:, 5]
-
-
-def calculate_density(arr):
-    weights = extract_weights(arr)
-    return sum(weights)
-
 
 # Load YOLO model
 model = YOLO("yolov8n.pt")
@@ -56,6 +49,12 @@ mask = cv2.imread("mask.png")
 
 # Create the object tracker
 tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
+
+# Dictionary to store time of entry for each tracked ID
+entry_times = {}
+
+# Dictionary to store waiting times for cars
+car_waiting_times = {}
 
 while True:
     success, img = cap.read()
@@ -79,16 +78,44 @@ while True:
             currentClass = CLASS_NAMES[cls]
 
             if currentClass in ["car", "truck", "bus", "motorbike"] and conf > 0.3:
-                cvzone.putTextRect(img, f'{currentClass} {conf}', (max(0, x1), max(35, y1)),
-                                   scale=4, thickness=3, offset=3)
-                cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=5)
                 currentArray = np.array([x1, y1, x2, y2, conf, get_weight(int(cls))])
                 detections = np.vstack((detections, currentArray))
 
     resultsTracker = tracker.update(detections)
 
-    # Display the density
-    cvzone.putTextRect(img, f' Density: {calculate_density(detections)}', (50, 100), 6)
+    for item in resultsTracker:
+        x1, y1, x2, y2, track_id = item[:5].astype(int)
+        currentClass = CLASS_NAMES[cls]
+        if currentClass in ["car", "truck", "bus", "motorbike"]:
+            # Check if this is a new ID
+            if track_id not in entry_times:
+                entry_times[track_id] = time.time()
+
+            # Get the time of entry
+            entry_time = entry_times[track_id]
+
+            # Calculate the elapsed time since entry
+            elapsed_time = time.time() - entry_time
+
+            # Display class name, tracking ID, and time of entry on the frame
+            cvzone.putTextRect(img, f'{currentClass} ID: {track_id}', (max(0, x1), max(35, y1)),
+                               scale=0, thickness=3, offset=3)
+            cvzone.putTextRect(img, f'Entry Time: {elapsed_time:.1f}s', (max(0, x1), max(80, y1)),
+                               scale=4, thickness=3, offset=2)
+            cvzone.cornerRect(img, (x1, y1, x2 - x1, y2 - y1), l=9, rt=5)
+
+            # Update the waiting time for cars
+            if currentClass == "car":
+                if track_id in car_waiting_times:
+                    car_waiting_times[track_id] = elapsed_time
+                else:
+                    car_waiting_times[track_id] = elapsed_time
+
+    # Calculate the cumulative waiting time for cars in the current detections
+    total_car_waiting_time = sum([car_waiting_times[id] for id in car_waiting_times if id in resultsTracker[:, 4].astype(int)])
+
+    # Display the cumulative waiting time for cars in the current detections
+    cvzone.putTextRect(img, f'Waiting Time (Current Detections): {total_car_waiting_time:.1f}s', (50, 150), 6)
 
     # Resize and display the image
     img_resized = cv2.resize(img, (960, 540))
